c7b4797cb9ff301363d243b0dbfeb42b88f68f27
EgorLis <lisnecro@gmail.com>
2020-06-19 15:19:33 +0300

    Исправил скрипт

--- First-Game/Assets/testCollider.cs
+++ First-Game/Assets/testCollider.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class testCollider : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
         print(collision.gameObject.name);
     }
