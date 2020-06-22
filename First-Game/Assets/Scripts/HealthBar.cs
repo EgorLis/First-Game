6a805d5c5c22b9b70b496d75ceed7820244d9c5b
EgorLis <lisnecro@gmail.com>
2020-06-22 14:49:45 +0300

    Добавил полоску ХП

---
+++ First-Game/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Scrollbar scrollbar;
+    private Creature person;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollbar.size = 1;
+        person = GameObject.FindGameObjectWithTag("Player").GetComponent<Creature>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("Player"))
+        {
+            scrollbar.size =  (float)person.m_health / (float)person.max_health;
+        }
+    }
+}
