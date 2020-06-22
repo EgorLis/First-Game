a874eed9a1bb41654f25e1ffe02c9a755b7a6591
EgorLis <lisnecro@gmail.com>
2020-06-22 11:01:05 +0300

    Добавил противника

---
+++ First-Game/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float MoveCoffi = 0.2f;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+            transform.position = Vector2.MoveTowards(transform.position, target.position, MoveCoffi * Time.deltaTime);
+    }
+}
