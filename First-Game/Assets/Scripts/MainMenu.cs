da08274ec8e5ea8e96f2c722fd4968368b8be226
Ciratoku <Ciratoku@users.noreply.github.com>
2020-06-22 14:30:36 +0300

    UserInterface очистка

--- First-Game/Assets/Scripts/MainMenu.cs
+++ First-Game/Assets/Scripts/MainMenu.cs
@@ -11,5 +11,9 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
